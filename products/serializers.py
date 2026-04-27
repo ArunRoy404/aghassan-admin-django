a188e81605e7cf236eb2caafd716b3ebe14ce663
unknown <rezanulraz62@gmail.com>
2026-04-27 16:18:20 +0600

    resolve 500 issue

--- products/serializers.py
+++ products/serializers.py
@@ -13,46 +13,52 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'thumbnail', 'created_at', 'mockup']
 
-        def get_mockup(self, obj):
-            request = self.context.get('request', None)
+    def get_mockup(self, obj):
+        request = self.context.get('request', None)
 
-            mockups = obj.psd_files.filter(psd_type='mockup')
-            res = []
+        mockups = obj.psd_files.filter(psd_type='mockup')
+        res = []
 
-            for m in mockups:
-                width, height = 0, 0
+        for m in mockups:
+            width = 0
+            height = 0
 
-                # SAFE structure_json handling
-                if isinstance(m.structure_json, dict):
-                    children = m.structure_json.get('children', [])
-                    for child in children:
-                        if isinstance(child, dict) and 'placedLayer' in child:
-                            pl = child.get('placedLayer', {})
-                            width = pl.get('width', 0)
-                            height = pl.get('height', 0)
-                            break
+            structure = m.structure_json
 
-                    if width == 0 and height == 0:
-                        width = m.structure_json.get('width', 0)
-                        height = m.structure_json.get('height', 0)
+            if isinstance(structure, dict):
+                children = structure.get('children', [])
+            elif isinstance(structure, list):
+                children = structure
+            else:
+                children = []
 
-                # SAFE image handling
-                thumbnail_url = None
-                if getattr(m, "image_without_warp", None):
-                    try:
-                        if request:
-                            thumbnail_url = request.build_absolute_uri(m.image_without_warp.url)
-                        else:
-                            thumbnail_url = m.image_without_warp.url
-                    except Exception:
-                        thumbnail_url = None
+            for child in children:
+                if isinstance(child, dict) and 'placedLayer' in child:
+                    placed = child.get('placedLayer', {})
+                    width = placed.get('width', 0)
+                    height = placed.get('height', 0)
+                    break
 
-                res.append({
-                    'id': m.id,
-                    'name': m.name,
-                    'width': width,
-                    'height': height,
-                    'thumbnail': thumbnail_url
-                })
+            if width == 0 and height == 0 and isinstance(structure, dict):
+                width = structure.get('width', 0)
+                height = structure.get('height', 0)
 
-            return res
+            thumbnail_url = None
+            if getattr(m, "image_without_warp", None):
+                try:
+                    if request:
+                        thumbnail_url = request.build_absolute_uri(m.image_without_warp.url)
+                    else:
+                        thumbnail_url = m.image_without_warp.url
+                except:
+                    thumbnail_url = None
+
+            res.append({
+                'id': m.id,
+                'name': m.name,
+                'width': width,
+                'height': height,
+                'thumbnail': thumbnail_url
+            })
+
+        return res
